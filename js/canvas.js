1d3804339e5d9b6a372699c9b0f6ca0a1bbeb6d7
David-Ostos <davidostos2@gmail.com>
2022-11-17 15:47:35 -0400

    agregando el canvas boton de inicio y de agg palabra, con la ejecucion de js para que paresca el los giones donde van a estar las letras correctas y la secciond e las letras incorrectas

--- js/canvas.js
+++ js/canvas.js
@@ -15,3 +15,51 @@ function dibujarCanvas(){
     tablero.closePath();
 
 }
+
+function dibujarLinea(){
+
+    tablero.lineWidth = 6; 
+    tablero.lineCap = "round";
+    tablero.lineJoin = "round";
+    tablero.fillStyle = "#F3F5F6";
+    tablero.STROKEsTYLE = "#8A3871";
+
+    let anchura = 600/palabraSecreta.length;
+    for(let i = 0; i < palabraSecreta.length; i++){
+        if(palabraSecreta.length <10){
+        tablero.moveTo(500 + (anchura*i), 640)
+        tablero.lineTo(550 + (anchura*i), 640)
+
+    } if (palabraSecreta.length > 10){
+        tablero.moveTo(500 + (anchura*i), 640)
+        tablero.lineTo(525 + (anchura*i), 640)
+
+    }
+    }
+
+    tablero.stroke();
+    tablero.closePath();
+}
+
+function escribirLetraCorrecta(index){
+
+    tablero.font = "bold 52px inter";
+    tablero.lineWidth = 6;
+    tablero.lineCap = "round";
+    tablero.lineJoin = "round";
+    tablero.fillStyle = "#F3F5F6";
+
+    let anchura = 600/palabraSecreta.length;
+    tablero.fillText(palabraSecreta[index],505+(anchura*index),620);
+    tablero.stroke();
+}
+
+function escribirLetraIncorrecta(letra,errorsLeft){
+    tablero.font = "bold 40px inter";
+    tablero.lineWidth = 6;
+    tablero.lineCap = "round";
+    tablero.lineJoin = "round";
+    tablero.fillStyle = "#F3F5F6";
+    tablero.fillText(letra, 535+(40*(10-errorsLeft)),710,40);
+
+}
--- js/horca.js
+++ js/horca.js
@@ -2,6 +2,8 @@
 let palabras = ["ALURA","ORACLE","COCIENCIA","ESFUERZO","COMPRENSION"]
 let tablero = document.getElementById("forca").getContext("2d");
 let palabraSecreta = "";
+let letras = [];
+let errores = 8;
 
 //palabraSecreta
 
@@ -11,6 +13,25 @@ function escojerPalabraSecreta (){
     console.log(palabraSecreta)
 }
 
+function comprobarLetra(key){ /* Se utiliza el parametro (key) para poder tomar las letras o numeros del teclado */
+    let estado = false;
+    if(key >= 65 && letras.indexOf(key) || key <= 90 && letras.indexOf(key)){/* aqui se sta haceindo una validacion para comprobar que las letras del alfabeto de la a-z en mayusculas */
+    letras.phus(key); /* para enpujar el valor */
+    console.log(key);
+    return estado; /* para devolver el valor*/
+    }else{
+        estado=true;
+        console.log(key);
+        return estado;
+    }
+
+}
+
+function anadirLetraIncorrecta(){
+    errores -=1;
+    console.log(errores)
+
+}
 //iniciar juego
 function iniciarJuego(){
 
@@ -18,4 +39,26 @@ function iniciarJuego(){
     
     escojerPalabraSecreta ();
     dibujarCanvas()
+    dibujarLinea()
+
+    document.onkeydown = (e) => { /* esta es una funcion con flecha espara decri que hay una funcion adentro de otra funcion y se representa  de esta manera => y aque se esat diceindo que va a funcionar con la tecla precionada del teclado se activa */
+        let letra= e.key.toUpperCase(); /* aqui es para poner la letras en mayusculas*/
+
+        if(comprobarLetra(letra) && palabraSecreta.includes(letra)){
+            for(let i = 0; i < palabraSecreta.length; i++) {
+                if(palabraSecreta[i] === letra) {
+                    escribirLetraCorrecta(i);
+               }
+        
+            }
+        }else{
+        anadirLetraIncorrecta(letra);
+        escribirLetraIncorrecta(letra,errores);}
+    }
+}
+
+function agregarPalabra(){
+
+    document.getElementById("desaparece").style.display = "none";
+
 }
