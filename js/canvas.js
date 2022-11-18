d997eb2fea1fae99c7ce80c6339385fe442599ef
David-Ostos <davidostos2@gmail.com>
2022-11-18 11:58:14 -0400

    terminando el challenge 2

--- js/canvas.js
+++ js/canvas.js
@@ -63,3 +63,73 @@ function escribirLetraIncorrecta(letra,errorsLeft){
     tablero.fillText(letra, 535+(40*(10-errorsLeft)),710,40);
 
 }
+
+
+function dibujarAhorcado(puntaje) {
+    tablero.lineWidth=8
+    tablero.lineCap="round"
+    tablero.lineJoin="round"
+    tablero.strokeStyle = "#0A3871"
+    if(puntaje===8){/*poste */
+        tablero.moveTo(700,500)
+        tablero.lineTo(700,100)
+        }
+    if(puntaje===7){/*soporte*/
+        tablero.moveTo(850,100)
+        tablero.lineTo(700,100)
+        }
+    if(puntaje===6){/*cuerda*/
+        tablero.moveTo(850,100)
+        tablero.lineTo(850,171)
+        }
+    if(puntaje===5){/*cara*/
+        tablero.moveTo(900,230)
+        tablero.arc(850,230,50,0,Math.PI*2)
+        }
+    if(puntaje===4){/*torzo*/
+        tablero.moveTo(850,389)
+        tablero.lineTo(850,289)
+        }
+    if(puntaje===3){/*pierna izquierda*/
+        tablero.moveTo(850,389)
+        tablero.lineTo(800,450)
+        }
+    if(puntaje===2){/*pierna derecha*/
+        tablero.moveTo(850,389)
+        tablero.lineTo(890,450)
+        }
+    if(puntaje===1){/* brazo izquierdo */
+        tablero.moveTo(850,330)
+        tablero.lineTo(800,389)
+        }
+    if(puntaje===0){ /*brazo derecho*/ 
+        tablero.moveTo(850,330)
+        tablero.lineTo(890,389)
+        }
+    tablero.stroke()
+    tablero.closePath()
+  }
+
+  function perdiste() {
+    tablero.font = ' bold 42px Inter';
+    tablero.lineWidth=6
+    tablero.lineCap="round"
+    tablero.lineJoin="round"
+    tablero.fillStyle="red"
+    tablero.fillText("Game Over!",930,320)
+  }
+
+  function ganaste() {
+    tablero.font = 'bold 42px Inter';
+    tablero.lineWidth=6
+    tablero.lineCap="round"
+    tablero.lineJoin="round"
+    tablero.fillStyle="green"
+    tablero.fillText("Ganaste,",950,320)
+    tablero.fillText("Bien jugado!",930,360)
+    setTimeout( recargar , 1000)
+  }   
+
+  function recargar(){
+    location.reload(); 
+  }
